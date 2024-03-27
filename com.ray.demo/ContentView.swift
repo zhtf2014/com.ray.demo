f1116c4a9a921ec49e625055bd7e799b38631db6
Ray Zhang <zhtf2014@hotmail.com>
2024-03-27 11:35:17 +0800

    Initial Commit

---
+++ com.ray.demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  com.ray.demo
+//
+//  Created by Ray on 2024/3/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ com.ray.demo/com_ray_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_ray_demoApp.swift
+//  com.ray.demo
+//
+//  Created by Ray on 2024/3/27.
+//
+
+import SwiftUI
+
+@main
+struct com_ray_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
